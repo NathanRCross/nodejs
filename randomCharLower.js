934538d7d95da7b982a9ddf9a4a91d4a5a3b32f1
NathanRCross <NRCross17@outlook.com>
2017-03-17 10:25:42 -0700

    Add files via upload

---
+++ randomCharLower.js
@@ -0,0 +1,22 @@
+//randomInCreate.js
+//binary random Integer 
+// save file as randomInCreate.js
+// to run type "node randomInCreate.js"
+var randomInteger = function(val) {
+	var ival = Math.random()*26+97;
+	//console.log(ival);
+	ival = Math.floor(ival);
+	return ival;
+}
+// main
+var i, rInt ;
+var letter = '';
+
+for (i = 0; i < 65536; i++){
+	rInt = randomInteger();
+	if (rInt === 97);
+	letter = String.fromCharCode(rInt);
+process.stdout.write(letter);
+
+}
+console.log(letter);
